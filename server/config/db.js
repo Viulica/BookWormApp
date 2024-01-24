d22b57034fab910da3eeb0131b71313d2568e939
Viulica <dermit.stela@gmail.com>
2024-01-24 19:31:08 +0100

    config baza

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 
 
-const sequelize = new Sequelize('BookWormAppDB', 'bookwormadmin', 'pass', {
-  host: 'localhost',
+const sequelize = new Sequelize('bookwormdb', 'admin', '5GXsuJcJ9Hr60hHEFDGKyKAMICtzglWZ', {
+  host: 'dpg-cmolc45a73kc73b7c890-a',
   dialect: 'postgres',
 });
 
