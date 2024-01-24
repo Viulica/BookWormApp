3375915a2fb352213588396bd89ec21d003af80f
Viulica <dermit.stela@gmail.com>
2024-01-24 18:58:36 +0100

    inbox

--- server/routes/tokenVerification.js
+++ server/routes/tokenVerification.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
    const token = req.headers.authorization;
-   console.log(token);
+   //console.log(token);
 
    if (!token) {
       return res.status(403).json({ message: 'Token nije pružen' });
